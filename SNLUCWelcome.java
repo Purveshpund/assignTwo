d575f958ab93306ae55829241b6478bf23230e34
Purveshpund <purvesh.pund.work@gmail.com>
2021-09-10 07:13:08 +0530

    welcome Commit

---
+++ SNLUCWelcome.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SNLUCWelcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to The Game of SANKE AND LADDER!!!");
+    }
+}
